22c004b74da30324fa8b7ad644b6be9b9a69bd85
nirav2000 <nirav2000+github@gmail.com>
2026-09-17 15:06:36 +0100

    Cache reusable app plugins offline

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
-const CACHE='open-days-v4';const ASSETS=['./','./index.html','./styles.css','./app.js','./hotfix.js','./data/schools.json','./data/enhancements.json','./calendar.ics','./manifest.webmanifest','./icon.svg'];
+const CACHE='open-days-v6';
+const ASSETS=[
+  './','./index.html','./styles.css','./app.js','./integrations.js',
+  './plugins/app-platform.js','./plugins/autosave.js','./plugins/shared-token-sync.js','./plugins/developer-notes.js','./plugins/version-lab.js',
+  './data/schools.json','./data/enhancements.json','./calendar.ics','./manifest.webmanifest','./icon.svg'
+];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))));
