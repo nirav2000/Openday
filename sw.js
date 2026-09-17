2a21d4043c257cc10dad777b4c780ef03735b9de
nirav2000 <nirav2000+github@gmail.com>
2026-09-17 14:22:44 +0100

    Fix iPhone calendar subscription and view

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE='open-days-v2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./data/schools.json','./data/enhancements.json','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+const CACHE='open-days-v4';const ASSETS=['./','./index.html','./styles.css','./app.js','./hotfix.js','./data/schools.json','./data/enhancements.json','./calendar.ics','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))));
