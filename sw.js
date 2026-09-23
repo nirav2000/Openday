99714983c785572358e9873d1dd1ee73e421f82d
nirav2000 <nirav2000+github@gmail.com>
2026-09-23 19:10:58 +0100

    Cache primary school data for 1.5.0

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE='open-days-v1.4.1';
+const CACHE='open-days-v1.5.0';
 const ASSETS=[
   './','./index.html','./styles.css','./app.js','./integrations.js','./version.json',
   './plugins/app-platform.js','./plugins/autosave.js','./plugins/firebase-token-sync.js','./plugins/developer-notes.js','./plugins/version-lab.js',
-  './data/schools.json','./data/enhancements.json','./calendar.ics','./manifest.webmanifest','./icon.svg'
+  './data/schools.json','./data/primary-schools.json','./data/enhancements.json','./calendar.ics','./manifest.webmanifest','./icon.svg'
 ];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
