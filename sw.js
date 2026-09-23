cf3788b84a207d8e4e1d03e3119d75a66590b41b
nirav2000 <nirav2000+github@gmail.com>
2026-09-23 21:11:26 +0100

    Cache Openday 1.6.0 personal updates

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='open-days-v1.5.1';
+const CACHE='open-days-v1.6.0';
 const ASSETS=[
   './','./index.html','./styles.css','./app.js','./integrations.js','./version.json',
-  './plugins/app-platform.js','./plugins/autosave.js','./plugins/firebase-token-sync.js','./plugins/developer-notes.js','./plugins/version-lab.js',
+  './plugins/app-platform.js','./plugins/autosave.js','./plugins/firebase-token-sync.js','./plugins/developer-notes.js','./plugins/version-lab.js','./plugins/personal-updates.js',
   './data/schools.json','./data/primary-schools.json','./data/enhancements.json','./calendar.ics','./manifest.webmanifest','./icon.svg'
 ];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
