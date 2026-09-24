9f69723e076071242405458e348254be4d08c49c
nirav2000 <nirav2000+github@gmail.com>
2026-09-24 08:58:36 +0100

    Refresh Openday 2.3.0 cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='open-days-v2.2.4';
+const CACHE='open-days-v2.3.0';
 const ASSETS=[
   './','./index.html','./styles.css','./app.js','./integrations.js','./version.json',
   './plugins/app-platform.js','./plugins/autosave.js','./plugins/firebase-token-sync.js','./plugins/developer-notes.js','./plugins/version-lab.js','./plugins/public-overrides.js','./plugins/personal-updates.js',
